78174d8197f442c2f1e8ca916447c7f219fefd72
Ayush Verma <iashverm@gmail.com>
2023-06-22 00:28:02 +0530

    UserDao

--- GMS-JEDI-2.0-Flipkart-Java-Project/src/com/flipkart/service/UserService.java
+++ GMS-JEDI-2.0-Flipkart-Java-Project/src/com/flipkart/service/UserService.java
@@ -1,8 +1,6 @@
-/**
- * 
- */
 package com.flipkart.service;
 import java.util.List;
+import com.flipkart.dao.UserGMSDaoImpl;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -12,36 +10,32 @@ import com.flipkart.bean.*;
  * 
  */
 public class UserService {
-	User user1 = new User("Ayush", "psw1", 3);
-	User user2 = new User("Naman", "psw2", 3);
-	User user3 = new User("Rahul", "psw3", 3);
-	List<User> userList = new ArrayList<>(Arrays.asList(user1, user2, user3));
-	List<Registration> registrationInfo = new ArrayList<Registration>();
-//	List<User> userList = new ArrayList<User>();
+//	User user1 = new User("Ayush", "psw1", 3);
+//	User user2 = new User("Naman", "psw2", 3);
+//	User user3 = new User("Rahul", "psw3", 3);
+//	List<User> userList = new ArrayList<>(Arrays.asList(user1, user2, user3));
+//	List<Registration> registrationInfo = new ArrayList<Registration>();
+////	List<User> userList = new ArrayList<User>();
 	public void registerCustomer(Registration customerData) {
-//		registrationInfo.add(customerData)
+		UserGMSDaoImpl userGMSDao = new UserGMSDaoImpl();
+		userGMSDao.registerCustomer(customerData);
 ;	}
 	
 	public void registerGymOwner(Registration ownerData) {
-		registrationInfo.add(ownerData);
+		UserGMSDaoImpl userGMSDao = new UserGMSDaoImpl();
+		userGMSDao.registerGymOwner(ownerData);
 	}
 	
 	public boolean authenticateUser(User userData) {
-		boolean authenticated = false;
-		
-		for(User us: userList)
-		{
-			if(userData.getUserName().equals(us.getUserName()) && userData.getPassword().equals(us.getPassword()) && userData.getRoleId() == (us.getRoleId())) {
-				authenticated = true;
-				break;
-			}
-		}
-		return authenticated;
+		UserGMSDaoImpl userGMSDao = new UserGMSDaoImpl();
+		return userGMSDao.isAuthenticated(userData);
 	}
 	
 	public void registerUser(User userData) {
-		userList.add(userData);
+		UserGMSDaoImpl userGMSDao = new UserGMSDaoImpl();
+		userGMSDao.registerUser(userData);
 	}
+	
 	public boolean logout(User userData) {
 		return true;
 	}

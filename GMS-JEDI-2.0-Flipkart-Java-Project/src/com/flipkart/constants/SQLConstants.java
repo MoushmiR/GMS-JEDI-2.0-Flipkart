61bf22c5ff27fbfbd51689a5c37bb1e264881b1e
Rahul S <rahulmysuru7@gmail.com>
2023-06-22 22:54:24 +0530

    error updates

--- GMS-JEDI-2.0-Flipkart-Java-Project/src/com/flipkart/constants/SQLConstants.java
+++ GMS-JEDI-2.0-Flipkart-Java-Project/src/com/flipkart/constants/SQLConstants.java
@@ -5,7 +5,7 @@ public class SQLConstants {
 	public static final String SQL_INSERT_GYM_DETAILS_QUERY = "insert into Gymnasium (gymOwnerEmail, name, address, totalArea, numItem, numSeatsPerSlot, isApproved) values(?, ?, ?, ?, ?, ?, ?)";
 	public static final String SQL_FETCH_GYMOWNER_DETAILS_QUERY = "select * from GymOwmer where email=?";
 	public static final String SQL_FETCH_GYM_DETAILS_QUERY ="select * from Gymnasium where gymOwnerEmail=?";
-	public static final String SQL_FETCH_ALL_GYM_DETAILS_QUERY ="select * from Gymnasium where gymOwnerEmail=?";
+	public static final String SQL_FETCH_ALL_GYM_DETAILS_QUERY ="select * from Gymnasium ";
 	public static final String SQL_AUTH_QUERY= "SELECT * FROM User WHERE email = ? AND password = ?";
 	public static final String SQL_INSERT_GYMOWNER_DETAILS_QUERY = "insert into GymOwner values(?,?, ?, ?,?, ?, ?,?, ?)";
 	public static final String SQL_INSERT_USER_DETAILS_QUERY = "insert into User values(?,?,?)";
